18399f3d8a82133e14cd2170385067e77beab8fc
Francisco Neto <netofrancisco.pro@gmail.com>
2026-04-24 15:50:48 -0300

    feat: create first stateless api using native http module

---
+++ node-studies/src/server.js
@@ -0,0 +1,16 @@
+import http from "node:http";
+
+const server = http.createServer((req, res) => {
+  const { method, url } = req;
+  if (method === "GET" && url === "/users") {
+    return res.end("Listagem de usuários");
+  }
+
+  if (method === "POST" && url === "/users") {
+    return res.end("Criando user");
+  }
+
+  return res.end("Hello World");
+});
+
+server.listen(3333);
