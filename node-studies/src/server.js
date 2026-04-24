066954620da05119c0ff58b11355fd29e6c9b216
Francisco Neto <netofrancisco.pro@gmail.com>
2026-04-24 18:14:04 -0300

    feat: make api stateful and status codes

--- node-studies/src/server.js
+++ node-studies/src/server.js
@@ -1,16 +1,25 @@
 import http from "node:http";
 
+const users = [];
 const server = http.createServer((req, res) => {
   const { method, url } = req;
+
   if (method === "GET" && url === "/users") {
-    return res.end("Listagem de usuários");
+    return res
+      .setHeader("Content-type", "application/json")
+      .end(JSON.stringify(users));
   }
 
   if (method === "POST" && url === "/users") {
-    return res.end("Criando user");
+    users.push({
+      id: 1,
+      name: "John Doe",
+      email: "johndoe@example.com",
+    });
+    return res.writeHead(201).end();
   }
 
-  return res.end("Hello World");
+  return res.writeHead(404).end();
 });
 
 server.listen(3333);
