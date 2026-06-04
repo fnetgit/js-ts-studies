ca0e56d88a22bcb9771dc02c16e82a722f9ed20e
Francisco Neto <netofrancisco.pro@gmail.com>
2026-06-04 15:44:43 -0300

    feat: implement dynamic count state in App component

--- projeto-react/primeiro-projeto-react/src/App.tsx
+++ projeto-react/primeiro-projeto-react/src/App.tsx
@@ -1,10 +1,15 @@
+import { useState } from "react";
 import { Display } from "./components/Display";
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <div>
       <h1>Contador de curtidas</h1>
-      <Display count={50} />
+      <Display count={count} />
+      <button onClick={() => setCount(count + 1)}>Curtir</button>
+      <button onClick={() => setCount(0)}>Resetar</button>
     </div>
   );
 }
