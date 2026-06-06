3d26660e9d373e7ac593d146dd3dca81cbe3507d
Francisco Neto <netofrancisco.pro@gmail.com>
2026-06-05 22:46:29 -0300

    feat: create cats app layout

---
+++ projetos-react/cats/cats/src/App.tsx
@@ -0,0 +1,22 @@
+function App() {
+  return (
+    <div>
+      <h1>Gerador de gatinhos</h1>
+
+      <div>
+        <p>Nenhuma imagem carregada</p>
+      </div>
+
+      <div>
+        <button>Like</button>
+        <button>Deslike</button>
+      </div>
+
+      <div>
+        <button>Gerar novo gatinho</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
