0c4aa5c11f6349b7bd855b6b6ad285b5697796db
Francisco Neto <netofrancisco.pro@gmail.com>
2026-06-05 22:46:36 -0300

    feat: fetch first cat image

--- projetos-react/cats/cats/src/App.tsx
+++ projetos-react/cats/cats/src/App.tsx
@@ -1,10 +1,27 @@
+import { useEffect, useState } from "react";
+
 function App() {
+  const [catUrl, setCatUrl] = useState<string | null>(null);
+
+  async function fetchCat() {
+    const response = await fetch(
+      "https://api.thecatapi.com/v1/images/search",
+    );
+    const data = await response.json();
+
+    setCatUrl(data[0].url);
+  }
+
+  useEffect(() => {
+    fetchCat();
+  }, []);
+
   return (
     <div>
       <h1>Gerador de gatinhos</h1>
 
       <div>
-        <p>Nenhuma imagem carregada</p>
+        {catUrl ? <img src={catUrl} alt="Gatinho" /> : <p>Carregando...</p>}
       </div>
 
       <div>
