f0cad391e585d1a1d13f756536cfb3e7dbdf1577
Francisco Neto <netofrancisco.pro@gmail.com>
2026-06-15 16:32:27 -0300

    feat: implement main layout structure

--- daisyui/DaisyShop/src/App.tsx
+++ daisyui/DaisyShop/src/App.tsx
@@ -1,7 +1,15 @@
 import "./App.css";
 
 function App() {
-  return <div></div>;
+  return (
+    <div className="min-h-screen bg-base-200 flex flex-col">
+      <Navbar />
+      <main className="flex flex-1 items-center justify-center p-4">
+        <SignupCard />
+      </main>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
