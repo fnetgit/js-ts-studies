3aaaecfe6d25f0fcac28b3ea84f1b96447060f46
Francisco Neto <netofrancisco.pro@gmail.com>
2026-06-17 16:14:53 -0300

    feat: add Footer component and integrate it into the App layout

--- daisyui/DaisyShop/src/App.tsx
+++ daisyui/DaisyShop/src/App.tsx
@@ -1,8 +1,8 @@
 import "./App.css";
+import { Footer } from "./components/Footer";
 import { Navbar } from "./components/Navbar";
 import { SignupCard } from "./components/SignupCard";
 
-//
 function App() {
   return (
     <div className="min-h-screen bg-base-200 flex flex-col">
@@ -10,7 +10,7 @@ function App() {
       <main className="flex flex-1 items-center justify-center p-4">
         <SignupCard />
       </main>
-      {/* <Footer /> */}
+      <Footer />
     </div>
   );
 }
---
+++ daisyui/DaisyShop/src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export function Footer() {
+  return (
+    <footer className="footer bg-base-100 text-base-content/60 p-4 footer-center">
+     <p>Feito na aula de DaisyUI</p>
+    </footer>
+  );
+}
