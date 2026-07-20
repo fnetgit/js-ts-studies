eac1f76936b7b52ebb501fee00d62488c152d2ce
Francisco Neto <netofrancisco.pro@gmail.com>
2026-07-20 17:10:27 -0300

    refactor(sinutri): separate back and front-end

--- sinutri/src/prisma.ts
+++
@@ -1,7 +0,0 @@
-import { PrismaLibSql } from "@prisma/adapter-libsql";
-import { PrismaClient } from "./generated/prisma/client.js";
-import { env } from "./config/env.js";
-
-const adapter = new PrismaLibSql({ url: env.databaseUrl });
-
-export const prisma = new PrismaClient({ adapter });
